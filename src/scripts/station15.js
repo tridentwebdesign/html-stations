1335ad2f42df691e7533db2ae45c4798e2e606c4
kawaguchi-trident <webdesigntrident@gmail.com>
2024-06-20 06:00:25 +0900

    追記

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,29 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
-}
+function test(data) {
+  data.forEach((obj) => {
+    obj.full_name = obj.family_name + " " + obj.first_name;
+  });
 
+  return new Promise((data) => setTimeout(data, 3000));
+}
